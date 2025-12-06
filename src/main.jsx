d18663a27deb1af47d058a466e8479d08af025d0
amilokz <amilokz1@gmail.com>
2025-12-06 10:57:56 -0800

    Remove old index.html

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // optional for JS component
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css'  // Make sure this line exists
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
