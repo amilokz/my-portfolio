19c49821776930cfd789bc1a17f94712a19b62e1
amilokz <amilokz1@gmail.com>
2025-12-07 04:22:09 -0800

    Setup GitHub Actions for React deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ⚠️ Use relative path instead of '/personal_portfolio/'
+  base: '/personal_portfolio/',  // your repo name
   plugins: [react()],
 })
